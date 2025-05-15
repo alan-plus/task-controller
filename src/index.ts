87b8e48b11073c9f52565272e711c4ec200ab92b
alan-plus <adgrosario@gmail.com>
2025-05-15 17:09:08 -0300

    fix index exports

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,11 @@
-import * as LockInterfaces from "./interfaces/lock";
-import * as TaskExecutorInterfaces from "./interfaces/task-executor";
 import { LockMutex } from "./locks/lock-mutex";
 import { LockPool } from "./locks/lock-pool";
 import { PromiseMutex } from "./task-executors/promise-mutex";
 import { PromisePool } from "./task-executors/promise-pool";
 import { PromiseMultiStep } from "./task-executors/promise-multi-step";
-import * as LockTypes from "./types/lock.type";
-import * as TaskExecutorTypes from "./types/task-executor.type";
 
-export {
-  LockInterfaces,
-  TaskExecutorInterfaces,
-  LockMutex,
-  LockPool,
-  PromiseMutex,
-  PromisePool,
-  PromiseMultiStep,
-  LockTypes,
-  TaskExecutorTypes,
-};
+export { LockMutex, LockPool, PromiseMutex, PromisePool, PromiseMultiStep };
+export * from "./types/lock.type";
+export * from "./types/task-executor.type";
+export * from "./interfaces/lock";
+export * from "./interfaces/task-executor";
